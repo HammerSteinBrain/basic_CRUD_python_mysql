2904d2ca6911162d9bff679859dd6d9dfc5261e5
Rodrigo Rosa <proff.rodrigo.rosa@gmail.com>
2018-10-01 21:57:48 -0300

    Initial commit

---
+++ create_table_operation.py
@@ -0,0 +1,19 @@
+#PYTHON 2.7
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("192.168.0.23","fatec","1234","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+print "Database version : %s " % data
+
+# disconnect from server
+db.close()
---
+++ delete_operation.py
@@ -0,0 +1,23 @@
+#python 2.7
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("192.168.0.25","fatec","1234","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to DELETE required records
+sql = "DELETE FROM EMPLOYEE WHERE AGE > '%d'" % (20)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
